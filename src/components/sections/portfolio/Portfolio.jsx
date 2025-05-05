8b74527645397475fdb9ff0f08a8d79c407b20dc
igorgp06 <igorgoncalvesp20@gmail.com>
2025-05-04 23:51:16 -0300

    [WORK 2.3] Atualização da seção de portfólio com novas animações, refatoração de componentes e ajustes de estilo responsivo.

--- src/components/sections/portfolio/Portfolio.jsx
+++ src/components/sections/portfolio/Portfolio.jsx
@@ -2,7 +2,7 @@ import "./portfolio.css";
 import { useEffect, useRef, useState } from "react";
 import { motion, useInView, useScroll, useTransform } from "motion/react";
 
-const portfolioItens = [
+const items = [
     {
         id: 1,
         img: "/static/images/DestinosdoKite.png",
@@ -40,46 +40,109 @@ const portfolioItens = [
     },
 ];
 
+const imgVariants = {
+    initial: {
+        x: -500,
+        y: 500,
+        opacity: 0,
+    },
+    animate: {
+        x: 0,
+        y: 0,
+        opacity: 1,
+        transition: {
+            duration: 0.5,
+            ease: "easeInOut",
+        },
+    },
+};
+
+const textVariants = {
+    initial: {
+        x: 500,
+        y: 500,
+        opacity: 0,
+    },
+    animate: {
+        x: 0,
+        y: 0,
+        opacity: 1,
+        transition: {
+            duration: 0.5,
+            ease: "easeInOut",
+            staggerChildren: 0.05,
+        },
+    },
+};
+
 const ListItem = ({ item }) => {
+    const ref = useRef();
+
+    const isInView = useInView(ref, { margin: "-100px" });
+
     return (
-        <div className="pItem">
-            <div className="pImg">
-                <img src={item.img} alt={item.desc} />
-            </div>
-            <div className="pText">
-                <h1>{item.title}</h1>
-                <p>{item.desc}</p>
-                <a href={item.link}>
+        <div className="pItem" ref={ref}>
+            <motion.div
+                variants={imgVariants}
+                animate={isInView ? "animate" : "initial"}
+                className="pImg"
+            >
+                <img src={item.img} alt="" />
+            </motion.div>
+            <motion.div
+                variants={textVariants}
+                animate={isInView ? "animate" : "initial"}
+                className="pText"
+            >
+                <motion.h1 variants={textVariants}>{item.title}</motion.h1>
+                <motion.p variants={textVariants}>{item.desc}</motion.p>
+                <motion.a variants={textVariants} href={item.link}>
                     <button>Veja o Projeto</button>
-                </a>
-            </div>
+                </motion.a>
+            </motion.div>
         </div>
-    )
-}
+    );
+};
 
 const Portfolio = () => {
-    
+    const [containerDistance, setContainerDistance] = useState(0);
     const ref = useRef(null);
 
-    const [containerDist, setContainerDist] = useState(0);
-
     useEffect(() => {
-        if(ref.current) {
-            const rect = ref.current.getBoundingClientRect();
-            setContainerDist(rect.left)
-        }
-    }, [])
+        const calculateDistance = () => {
+            if (ref.current) {
+                const rect = ref.current.getBoundingClientRect();
+                setContainerDistance(rect.left);
+            }
+        };
+
+        calculateDistance();
+
+        window.addEventListener("resize", calculateDistance);
+
+        return () => {
+            window.removeEventListener("resize", calculateDistance);
+        };
+    }, []);
 
     const { scrollYProgress } = useScroll({ target: ref });
 
-    const xTranslate = useTransform(scrollYProgress, [0, 1], [0, -window.innerWidth * portfolioItens.length])
+    const xTranslate = useTransform(
+        scrollYProgress,
+        [0, 1],
+        [0, -window.innerWidth * items.length]
+    );
 
     return (
         <div className="portfolio" ref={ref}>
             <motion.div className="pList" style={{ x: xTranslate }}>
-                <div className="empty" style={{ width:window.innerWidth - containerDist }}/>
-                    
-                {portfolioItens.map(item => (
+                <div
+                    className="empty"
+                    style={{
+                        width: window.innerWidth - containerDistance,
+                    }}
+                />
+                {items.map((item) => (
                     <ListItem item={item} key={item.id} />
                 ))}
             </motion.div>
@@ -88,8 +151,30 @@ const Portfolio = () => {
             <section />
             <section />
             <section />
-        </div >
-    )
-}
+            <div className="pProgress">
+                <svg width="100%" height="100%" viewBox="0 0 160 160">
+                    <circle
+                        cx="80"
+                        cy="80"
+                        r="70"
+                        fill="none"
+                        stroke="#f2efeb"
+                        strokeWidth={20}
+                    />
+                    <motion.circle
+                        cx="80"
+                        cy="80"
+                        r="70"
+                        fill="none"
+                        stroke="#ae7ef2"
+                        strokeWidth={20}
+                        style={{ pathLength: scrollYProgress }}
+                        transform="rotate(-90 80 80)"
+                    />
+                </svg>
+            </div>
+        </div>
+    );
+};
 
-export default Portfolio
+export default Portfolio;
