f9d232ffce9ba4fc6bb4919a2cfcea653a5c02c2
igorgp06 <igorgoncalvesp20@gmail.com>
2025-07-21 23:19:22 -0300

    [WORK 4.3] Conclusão da seção footer

--- src/components/partials/footer/Footer.jsx
+++ src/components/partials/footer/Footer.jsx
@@ -1,11 +1,18 @@
-
+import { ArrowUp } from "lucide-react"
 
 export const Footer = () => {
 
     return (
-        <footer>
+        <footer className="py-6 px-6 relative p-8 flex flex-wrap justify-between items-center">
             {" "}
-            <p> </p>
+            <p className="text-sm text-muted-foreground">
+                {" "}
+                &copy; {new Date().getFullYear()} Igor Gonçalves | DEV <br />Todos os direitos reservados.
+            </p>
+
+            <a href="#hero" className="p-2 rounded-full bg-primary/10 hover:bg-primary/20 transition-colors duration-300">
+                <ArrowUp />
+            </a>
         </footer>
     )
 }
