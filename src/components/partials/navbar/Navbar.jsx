ccc24eb221c33e841125bcc0d4bfa1b8e633017f
igorgp06 <igorgoncalvesp20@gmail.com>
2025-07-22 23:45:09 -0300

    [WORK 4.6] Melhorias na responsividade do header

--- src/components/partials/navbar/Navbar.jsx
+++ src/components/partials/navbar/Navbar.jsx
@@ -16,77 +16,94 @@ export const Navbar = () => {
 
     useEffect(() => {
         const handleScroll = () => {
-            setIsScrolled(window.screenY > 10);
+            setIsScrolled(window.scrollY > 10);
         };
 
         window.addEventListener("scroll", handleScroll);
         return () => window.removeEventListener("scroll", handleScroll);
     }, []);
+
+    useEffect(() => {
+        if (isMenuOpen) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "auto";
+        }
+
+        return () => {
+            document.body.style.overflow = "auto";
+        };
+
+    }, [isMenuOpen]);
+
     return (
-        <nav
-            className={cn(
-                "fixed w-full border-b left-0 top-0 z-40 transition-all duration-300",
-                isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
-            )}
-        >
-            <div className="container flex items-center justify-between">
-                <a
-                    className="text-xl font-bold text-primary flex items-center"
-                    href="#hero"
-                >
-                    <span className="relative z-10">
-                        <span className="text-glow text-foreground"> Igor Gonçalves </span>{" "}
-                        | DEV
-                    </span>
-                </a>
+        <>
+            <nav
+                className={cn(
+                    "fixed w-full left-0 top-0 z-40 transition-all duration-300",
+                    isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs border-b" : "py-5"
+                )}
+            >
+                <div className="container flex items-center justify-between">
+                    <a
+                        className="text-xl font-bold text-primary flex items-center"
+                        href="#hero"
+                        onClick={() => setIsMenuOpen(false)}
+                    >
+                        <span className="relative z-10">
+                            <span className="text-glow text-foreground"> Igor Gonçalves </span>{" "}
+                            | DEV
+                        </span>
+                    </a>
 
-                {/* desktop */}
+                    {/* desktop */}
 
-                <div className="hidden md:flex space-x-8">
-                    {navItems.map((item, key) => (
-                        <a
-                            key={key}
-                            href={item.href}
-                            className="text-foreground/80 hover:text-primary transition-colors duration-300"
-                        >
-                            {item.name}
-                        </a>
-                    ))}
-                </div>
-                
-                    {/* mobile */}
-
-                <button
-                    onClick={() => setIsMenuOpen((prev) => !prev)}
-                    className="md:hidden p-2 text-foreground z-50"
-                    aria-label={isMenuOpen ? "Close Menu" : "Open Menu"}
-                >
-                    {isMenuOpen ? <X size={24} /> : <Menu size={24} />}{" "}
-                </button>
-
-                <div
-                    className={cn(
-                        "fixed inset-0 bg-background/95 backdroup-blur-md z-40 flex flex-col items-center justify-center",
-                        "transition-all duration-300 md:hidden",
-                        isMenuOpen
-                            ? "opacity-100 pointer-events-auto"
-                            : "opacity-0 pointer-events-none"
-                    )}
-                >
-                    <div className="flex flex-col space-y-8 text-xl">
+                    <div className="hidden md:flex space-x-8">
                         {navItems.map((item, key) => (
                             <a
                                 key={key}
                                 href={item.href}
-                                className="text-foreground/80 hover:text-primary transition-colors duration-300"
-                                onClick={() => setIsMenuOpen(false)}
+                                className="text-foreground hover:text-primary transition-colors duration-300"
                             >
                                 {item.name}
                             </a>
                         ))}
                     </div>
                 </div>
+            </nav>
+
+            {/* mobile */}
+
+            <button
+                onClick={() => setIsMenuOpen((prev) => !prev)}
+                className="md:hidden p-2 text-foreground z-50 fixed top-2 right-4"
+                aria-label={isMenuOpen ? "Close Menu" : "Open Menu"}
+            >
+                {isMenuOpen ? <X size={24} /> : <Menu size={24} />}{" "}
+            </button>
+
+            <div
+                className={cn(
+                    "fixed inset-0 bg-background/99 backdroup-blur-md z-49 flex flex-col items-center justify-center",
+                    "transition-all duration-300 md:hidden",
+                    isMenuOpen
+                        ? "opacity-100 pointer-events-auto"
+                        : "opacity-0 pointer-events-none"
+                )}
+            >
+                <div className="flex flex-col space-y-8 text-xl text-foreground w-full items-center h-full justify-center ">
+                    {navItems.map((item, key) => (
+                        <a
+                            key={key}
+                            href={item.href}
+                            className="text-foreground hover:text-primary transition-colors duration-300"
+                            onClick={() => setIsMenuOpen(false)}
+                        >
+                            {item.name}
+                        </a>
+                    ))}
+                </div>
             </div>
-        </nav>
+        </>
     );
 };
